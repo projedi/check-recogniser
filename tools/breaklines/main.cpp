9a7ba6f00ad1dacbf9e4a21a7448f4c979370cbb
Alexander Shabalin <shabalyn.a@gmail.com>
2012-11-14 02:52:59 +0400

    Almost working breaking into lines

--- tools/breaklines/main.cpp
+++ tools/breaklines/main.cpp
@@ -35,6 +35,31 @@ void cropCheck(Mat& src) {
    getRectSubPix(rotated, box.size, box.center, src);
 }
 
+void sortAndMergeRects(vector<Rect>& rects) {
+   //TODO: sort by line number
+   for(int i = 0; i != rects.size(); i++) {
+      Rect& r1 = rects[i];
+      if(r1.width == 0 && r1.height == 0) continue;
+      for(int j = i+1; j != rects.size(); j++) {
+         Rect& r2 = rects[j];
+         if(r2.width == 0 && r2.height == 0) continue;
+         int xdiff = (r1.x + r1.width/2) - (r2.x + r2.width/2);
+         int ydiff = (r1.y + r1.height/2) - (r2.y + r2.height/2);
+         if(ydiff <= 15) {
+            //if(  r1.contains(Point(r2.x+r2.width/2,r2.y+r2.height/2))
+            //  || r2.contains(Point(r1.x+r1.width/2,r1.y+r1.height/2))) {
+               r1 |= r2;
+               rects.erase(rects.begin()+j);
+               j--;
+               //r2.x = 0;
+               //r2.y = 0;
+               //r2.width = 0;
+               //r2.height = 0;
+            }
+      }
+   }
+}
+
 void retrieveBoundingBoxes(Mat& src) {
    Mat img = src.clone();
    cvtColor(src,src,CV_GRAY2BGR);
@@ -48,36 +73,58 @@ void retrieveBoundingBoxes(Mat& src) {
    Mat hierarchy;
    findContours(img, contours, hierarchy, CV_RETR_LIST, CV_CHAIN_APPROX_SIMPLE);
    cout << contours.size() << endl;
-   //vector< vector<Point> > rects;
+   vector<Rect> rects;
+   vector< vector<Point> > squares;
    for(int i = 0; i != contours.size(); i++) {
       Point2f center;
       float radius;
       minEnclosingCircle(contours[i], center, radius);
-      if(radius < 5 || radius > 100) continue;
-      Mat mat;
-      getRectSubPix(src, Size(2*radius, 2*radius), center, mat);
-      imshow("Letters", mat);
-      waitKey(0);
+      if(radius < 5 || radius > 60) continue;
+      //Mat mat;
+      //getRectSubPix(src, Size(2*radius, 2*radius), center, mat);
+      //imshow("Letters", mat);
+      //waitKey(0);
+      Rect rect((int)(center.x-radius),(int)(center.y-radius),(int)(2*radius),(int)(2*radius));
+      rects.push_back(rect);
       /*
       vector<Point> points;
       points.push_back(Point((int)(center.x - radius), (int)(center.y - radius)));
       points.push_back(Point((int)(center.x + radius), (int)(center.y - radius)));
       points.push_back(Point((int)(center.x + radius), (int)(center.y + radius)));
       points.push_back(Point((int)(center.x - radius), (int)(center.y + radius)));
-      rects.push_back(points);
+      squares.push_back(points);
       */
    }
-   //cout << rects.size() << endl;
-   //drawContours(src, rects, -1, Scalar(0, 0, 255), 2, 8);
+   cout << rects.size() << endl;
+   //TODO: Somehow there're disjoined rects.
+   sortAndMergeRects(rects);
+   //groupRectangles(rects, 1, 0.2);
+   cout << rects.size() << endl;
+   for(int i = 0; i != rects.size(); i++) {
+      Rect r = rects[i];
+      Mat mat;
+      getRectSubPix(src, Size(r.width, r.height), Point(r.x+r.width/2, r.y+r.height/2), mat);
+      //imshow("Letters", mat);
+      //waitKey(0);
+      vector<Point> points;
+      points.push_back(Point(r.x, r.y));
+      points.push_back(Point(r.x+r.width, r.y));
+      points.push_back(Point(r.x+r.width, r.y+r.height));
+      points.push_back(Point(r.x, r.y+r.height));
+      squares.push_back(points);
+   }
+   drawContours(src, squares, -1, Scalar(0, 0, 255), 2, 8);
    displayImg(src,500);
 }
 
 void solve(string filename) {
    Mat src = imread(filename, 0);
    //displayImg(src);
-   cropCheck(src);
+   //cropCheck(src);
    displayImg(src,500);
    retrieveBoundingBoxes(src);
+   //TODO: After that use tesseract to convert to text.
+   //Then parse this text like on a demo. Pray.
 }
 
 int main(int argc, char** argv) {
